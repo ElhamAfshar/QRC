9c7b7cad25e73ec8c96d77b45d4811a719047968
MissNull <elhamafshar2100@gmail.com>
2020-11-14 10:55:08 +0330

    add lat_long

---
+++ Src/Schemas/province.ts
@@ -0,0 +1,23 @@
+import type { ObjectId } from "https://deno.land/x/mongo@v0.12.1/mod.ts";
+import db from "../utils/db";
+
+export interface LatLong {
+  lat: string;
+  long: string;
+}
+
+export interface Province {
+  _id: ObjectId;
+  name: string;
+  enName: string;
+  coordinates: LatLong[];
+}
+
+export interface RProvince {
+  _id?: 0 | 1;
+  name?: 0 | 1;
+  enName?: 0 | 1;
+  coordinates?: LatLong[];
+}
+
+export const provinces = db.collection<Province>("Provinces");
