9f723cf16688be4bb1cbc576ffd8ed95c1409df2
zsattari <famil.shia14@gmail.com>
2020-11-14 03:38:15 -0500

    ntn

--- Src/Controllers/Province/Funcs/add.ts
+++ Src/Controllers/Province/Funcs/add.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from "https://deno.land/x/mongo@v0.12.1/mod.ts";
-import { creating } from "../../../cfns/create.ts";
+import { creating } from "../../../Cfns/create.ts";
 import { Province, RProvince } from "../../../Schemas/province.ts";
-import { throwError } from "../../../utils/Function/throwErr.ts";
-import type { details } from "../../../utils/TypeScript/commonTypes.ts";
+import { throwError } from "../../../Utils/Function/throwErr.ts";
+import type { details } from "../../../Utils/TypeScript/commonTypes.ts";
